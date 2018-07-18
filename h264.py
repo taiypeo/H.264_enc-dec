c8b8b43f2d8449058b6da857e33337e320b23d37
QwertygidQ <Qwertygid@gmail.com>
2018-07-18 16:10:30 +0300

    Added get_yuv420_frame()

--- h264.py
+++ h264.py
@@ -2,32 +2,45 @@ import ffmpeg
 
 # Decodes any video file "in_filename" to a rawvideo file "out_filename"
 def h264_decode(in_filename, out_filename, **ffmpeg_kwargs):
-    (
+    return (
         ffmpeg
         .input(in_filename)['v']
         .output(out_filename, vcodec='rawvideo', map='0:v:0', **ffmpeg_kwargs)
-        .run()
-    )
+        .run(capture_stdout=True)
+    )[0]
 
 # Encodes any video file "in_filename" to a H.264 file "out_filename"
 def h264_encode(in_filename, out_filename, **ffmpeg_kwargs):
-    (
+    return (
         ffmpeg
         .input(in_filename)
         .output(out_filename, vcodec='libx264', map='0:v:0', **ffmpeg_kwargs)
-        .run()
-    )
+        .run(capture_stdout=True)
+    )[0]
 
 # Takes frame #frame_num in any video file "in_video_filename" and saves it
 # as a JPEG file "out_jpeg_filename"
 def get_jpeg_frame(in_video_filename, out_jpeg_filename, frame_num):
-    (
+    return (
         ffmpeg
         .input(in_video_filename)
         .filter('select', 'gte(n, {})'.format(frame_num))
         .output(out_jpeg_filename, vframes=1, format='image2', vcodec='mjpeg')
-        .run()
-    )
+        .run(capture_stdout=True)
+    )[0]
+
+# Takes frame #frame_num in any video file "in_video_filename" and saves it
+# as a YUV file "out_yuv_filename"
+def get_yuv420_frame(in_video_filename, out_yuv_filename, frame_num):
+    width, height = get_video_frame_size(in_video_filename)
+
+    return (
+        ffmpeg
+        .input(in_video_filename)
+        .filter('select', 'gte(n, {})'.format(frame_num))
+        .output(out_yuv_filename, vframes=1, format='rawvideo', pixel_format='yuv420p', s=str(width) + 'x' + str(height))
+        .run(capture_stdout=True)
+    )[0]
 
 # Returns a dictionary with all of the available information about the file
 def get_file_info(in_filename):
